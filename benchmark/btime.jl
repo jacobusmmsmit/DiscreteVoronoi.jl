34158ee35bf4e7ba5fbf553507ab81417281be85
goerch <jhr.walter@t-online.de>
2022-03-16 15:50:04 +0100

    Bugfix and benchmark for MvNormal

--- benchmark/btime.jl
+++ benchmark/btime.jl
@@ -1,6 +1,7 @@
 include("../src/DiscreteVoronoi.jl")
 using .DiscreteVoronoi
 using Random
+using Distributions
 using BenchmarkTools
 
 function rand_sites(::Type{Int}, N, M, K)
@@ -9,7 +10,11 @@ function rand_sites(::Type{Int}, N, M, K)
     idx[1:K]
 end
 
-for n in [100, 1000]
+function rand_sites(::Type{T}, N, M, K) where {T<:AbstractFloat}
+    [rand(MvNormal((N / 4, 3 * M / 4), (N / 8,M / 8))) for k in 1:K]
+end
+
+#= for n in [100, 1000]
     for s in [isqrt(n), n, n * isqrt(n), n * n]
         @show n, s
         println("jfa")
@@ -31,6 +36,34 @@ for n in [100, 1000]
                      jdac_aux3a!, jdac_aux3c!, 
                      jdac_aux4a!, jdac_aux4c!, 
                      jdac_aux5a!, jdac_aux5c!]
+            @show aux!
+            @btime jdacx!(grid, sites, $aux!) setup = (
+                Random.seed!(42);
+                grid = zeros(Int, $n, $n);
+                sites = collect(enumerate(rand_sites(Int, size(grid)..., $s))))
+        end
+    end
+end =#
+
+for n in 1000:1000:5000
+    for s in [n, n * isqrt(n)]
+        @show n, s
+
+        #= println("dac!")
+        @btime dac!(grid, sites) setup=(
+            Random.seed!(42);
+            grid = zeros(Int32, $n, $n);
+            sites = rand_sites(size(grid)..., $s)) =#
+
+        println("jdac")
+        # Not testing jdac_aux0! due to poor scaling in s (number of sites)
+        for aux! in [# jdac_aux1a!, jdac_aux1c!, 
+                     # jdac_aux2a!, jdac_aux2c!, 
+                     # jdac_aux3a!, 
+                     jdac_aux3c!, 
+                     # jdac_aux4a!, jdac_aux4c!, 
+                     # jdac_aux5a!, 
+                     jdac_aux5c!]
             @show aux!
             @btime jdacx!(grid, sites, $aux!) setup = (
                 Random.seed!(42);
