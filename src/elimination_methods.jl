e0c655a3b3912162d64bf7128644151946053891
Jacobus Smit <jacobusmmsmit@gmail.com>
2022-10-03 01:02:20 +0200

    Remove unnecessary functions, optimize condition

--- src/elimination_methods.jl
+++ src/elimination_methods.jl
@@ -1,6 +1,8 @@
 function exact_condition(site, sites, TL, BR)
     for other_site in sites
-        site == other_site && continue
+        if site == other_site
+            continue
+        end
         if all(distance(site, corner) > distance(other_site, corner) for corner in get_corners(TL, BR))
             return false
         end
@@ -12,29 +14,5 @@ function centre_anchor_condition(site, sites, TL, BR)
     centre = @. TL + BR / 2
     anchor = find_closest_site(centre, sites)
     corners = get_corners(TL, BR)
-    for corner in corners
-        distance(site, corner) <= distance(anchor, corner) && return true
-    end
-    return false
-end
-
-function exact_elimination(sites, TL, BR)
-    mask = SVector{length(sites),Bool}(exact_condition(site, sites, TL, BR) for site in sites)
-    return view(sites, mask)
-end
-
-function centre_anchor_elimination(sites, TL, BR)
-    centre = @. TL + BR / 2
-    anchor = find_closest_site(centre, sites)
-    corners = get_corners(TL, BR)
-
-    function cond(site)
-        for corner in corners
-            distance(site, corner) < distance(anchor, corner) && return true
-        end
-        return false
-    end
-
-    mask = SVector{length(sites),Bool}([cond(site) for site in sites])
-    return view(sites, mask)
+    return any(distance(site, corner) <= distance(anchor, corner) for corner in corners)
 end
