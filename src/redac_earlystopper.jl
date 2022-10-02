380d3291ff76521439619427f775b33e6b24870c
Jacobus Smit <jacobusmmsmit@gmail.com>
2022-10-02 20:05:10 +0200

    Rearrange files

--- src/redac_earlystopper.jl
+++ src/redac_earlystopper.jl
@@ -1,15 +1,3 @@
-using LinearAlgebra: norm
-using StaticArrays
-using BenchmarkTools
-
-include("helper_functions.jl")
-include("elimination_methods.jl")
-include("traditional_approaches.jl")
-
-# A key part of the implementation is the monadic (?) EarlyStopper type which
-# wraps information about how far an iterator should go in an object.
-include("EarlyStopper.jl")
-
 # Reduce, Divide, and Conquer
 function early_stop_sort!(early_stopper::ES, predicate) where {ES<:EarlyStopper}
     N = length(early_stopper)
@@ -58,39 +46,3 @@ function _redac_voronoi!(grid, TL, BR, sites::ES, p, predicate) where {ES<:Early
     end
     return nothing
 end
-
-### Example ###
-n = 50
-l = 3
-TL = (1, 1)
-BR = (n, n)
-locs = sort([SVector{2}(rand(1:n, 2)) for _ in 1:l])
-# sites = [Site(loc, lab) for (lab, loc) in enumerate(locs)]
-grid = zeros(Int, (n, n))
-loc_grid = zeros(SVector{2,Int}, (n, n))
-
-begin
-    dac_time = Float64[]
-    redac_time = Float64[]
-    redac_anchor_time = Float64[]
-    naive_time = Float64[]
-    ls = 1:100
-    for l in ls
-        locs = sort([SVector{2}(rand(1:n, 2)) for _ in 1:l])
-        for (alg, timer) in zip((dac_voronoi!, (x, y) -> redac_voronoi!(x, y, predicate=exact_condition), (x, y) -> redac_voronoi!(x, y, predicate=centre_anchor_condition), naive_voronoi!), (dac_time, redac_time, redac_anchor_time, naive_time))
-            push!(timer, @elapsed(alg(loc_grid, locs)))
-        end
-    end
-end
-using Plots
-plot(xlabel="Number of sites", ylabel="Time (s)", legendposition=:topleft)
-plot!(ls, dac_time, label="DAC")
-plot!(ls[2:end], redac_time[2:end], label="REDAC + Exact condition")
-plot!(ls[2:end], redac_anchor_time[2:end], label="REDAC + Centre-Anchor condition")
-plot!(ls, naive_time, label="Naive")
-
-@belapsed redac_voronoi!($loc_grid, $locs, predicate=exact_condition)
-@belapsed redac_voronoi!($loc_grid, $locs, predicate=centre_anchor_condition)
-@belapsed dac_voronoi!($loc_grid, $locs)
-@belapsed naive_voronoi!($loc_grid, $locs)
-@belapsed jfa_voronoi!($loc_grid, $locs)
