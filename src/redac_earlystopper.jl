967ce7f3359e22ff3882e114db8cb3b61967bbac
Jacobus Smit <jacobusmmsmit@gmail.com>
2022-10-03 17:03:59 +0200

    Make implementation more similar to dac_

--- src/redac_earlystopper.jl
+++ src/redac_earlystopper.jl
@@ -12,12 +12,12 @@ end
 site_sort!(sites, keep_predicate) = site_sort!(EarlyStopper(sites), keep_predicate)
 
 """
-    redac_voronoi!(grid, sites; p=2, predicate=exact_condition)
+    redac_voronoi!(grid, sites::T; p=2, predicate=exact_condition)  where {T<:Vector{SVector{2,Int}}}
 
 Performs a divide-and-conquer method similar to `dac_voronoi!` but has an additional site-elimination
 step which aims to reduce the work of subsequent steps.
 """
-function redac_voronoi!(grid, sites; p=2, predicate=exact_condition)
+function redac_voronoi!(grid, sites::T; p=2, predicate=exact_condition) where {T<:Vector{SVector{2,Int}}}
     TL = 1, 1
     BR = size(grid)
     _redac_voronoi!(grid, TL, BR, EarlyStopper(sites), p, predicate)
@@ -25,12 +25,12 @@ function redac_voronoi!(grid, sites; p=2, predicate=exact_condition)
 end
 
 function _redac_voronoi!(grid, TL, BR, sites::ES, p, predicate) where {ES<:EarlyStopper}
-    # Again, first, if the grid is a single cell, we are done
+    # First, if the grid is a single cell then we are done
     if all((BR .- TL) .== 0)
         grid[TL...] = find_closest_site(TL, sites, p)
     else
         # Otherwise we check if all corners have the same closest site
-        corners = (TL, (TL[1], BR[2]), (BR[1], TL[2]), BR)
+        corners = get_corners(TL, BR)
         closest_corners = (find_closest_site(corner, sites, p) for corner in corners)
         if allequal(closest_corners)
             view(grid, TL[1]:BR[1], TL[2]:BR[2]) .= Ref(first(closest_corners))
