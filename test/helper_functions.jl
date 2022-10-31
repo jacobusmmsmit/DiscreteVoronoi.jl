e2e72e79c7e75e6040c670d9c9a0c580fb6bee54
goerch <jhr.walter@t-online.de>
2022-10-31 07:59:12 +0100

    Benchmark adjustments

--- test/helper_functions.jl
+++ test/helper_functions.jl
@@ -3,5 +3,5 @@ function random_coordinates(N, K)
 end
 
 function random_coordinates(N, M, K)
-    Coord.(shuffle!([Iterators.product(1:N, 1:M)...])[1:min(M * N, K)])
+    Coord.(shuffle!([Iterators.product(1:N, 1:M)...])[1:min(N * M, K)])
 end
